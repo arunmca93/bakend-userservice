24a8131be017a6f95e129657341b94005fd47b5a
Arunkumar G <arunmca93@gmail.com>
2026-04-06 00:32:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 // ========== CORS ==========
 app.use(cors({
-  origin: [
-    'https://amznpro.online',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'http://127.0.0.1:8080',
-    'http://localhost:8080'
-  ],
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
